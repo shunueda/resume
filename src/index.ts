e27a2459107f1760327ec42f6679d6dade252ad3
Shun Ueda <me@shu.nu>
2024-06-30 19:35:23 -0700

    rollback

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,16 @@
-import { DefaultArtifactClient } from '@actions/artifact'
 import * as core from '@actions/core'
+import { GoogleGenerativeAI } from '@google/generative-ai'
+import { config } from 'package.json'
 
-// const generativeModel = new GoogleGenerativeAI(
-//   process.env.GOOGLE_API_KEY
-// ).getGenerativeModel({
-//   model: config.model
-// })
-//
-// const prompt = ['Hello, how are you?']
+const generativeModel = new GoogleGenerativeAI(
+  process.env.GOOGLE_API_KEY
+).getGenerativeModel({
+  model: config.model
+})
 
-// const result = await generativeModel.generateContent(prompt)
-// const output = result.response.text()
+const prompt = ['Hello, how are you?']
 
-const artifact = new DefaultArtifactClient()
-await artifact.uploadArtifact('test-artifact', ['tsconfig.json'], '.')
+const result = await generativeModel.generateContent(prompt)
+const output = result.response.text()
 
-core.info('Hello, world!')
+core.info(output)
