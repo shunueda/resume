9438652a8e023947c316534c83753027c06f1222
Shun Ueda <me@shu.nu>
2024-06-30 19:03:30 -0700

    try bare run

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,18 @@
-import { GoogleGenerativeAI } from '@google/generative-ai'
-import { config } from 'package.json'
+import { DefaultArtifactClient } from '@actions/artifact'
+import * as core from '@actions/core'
 
-const generativeModel = new GoogleGenerativeAI(
-  process.env.GOOGLE_API_KEY
-).getGenerativeModel({
-  model: config.model
-})
+// const generativeModel = new GoogleGenerativeAI(
+//   process.env.GOOGLE_API_KEY
+// ).getGenerativeModel({
+//   model: config.model
+// })
+//
+// const prompt = ['Hello, how are you?']
 
-const prompt = ['Hello, how are you?']
+// const result = await generativeModel.generateContent(prompt)
+// const output = result.response.text()
 
-const result = await generativeModel.generateContent(prompt)
-console.log(result.response.text())
+const artifact = new DefaultArtifactClient()
+await artifact.uploadArtifact('test-artifact', ['tsconfig.json'], '.')
+
+core.info('Hello, world!')
